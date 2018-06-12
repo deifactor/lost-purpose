d4a99b9e106f57d7056575c911727c7f8d73c008
Ash <relativistic.policeman@gmail.com>
2018-06-12 01:19:10 -0700

    Add a hello-world frontend.

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CheckerPlugin()
+    ]
+};
