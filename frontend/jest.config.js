448e2e5fd9000ff3989e0e8fe8d6d8a7a4eeade8
Ash <relativistic.policeman@gmail.com>
2018-08-08 01:56:47 -0700

    jest.config.js workaround; see facebook/jest#6766

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
+  "testURL": "http://localhost",
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   "moduleFileExtensions": [
     "ts",
