e4233997f864df35a85aaf583d5fbfa8a306e317
Ash <relativistic.policeman@gmail.com>
2018-08-17 23:54:48 -0700

    Use option parsing for the binary.

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,18 +1,41 @@
 extern crate ascender;
 extern crate image;
 extern crate rusttype;
+#[macro_use]
+extern crate structopt;
 
-use std::env;
-use std::fs;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cli")]
+struct Opt {
+    /// The image to render as ASCII.
+    #[structopt(name = "FILE", parse(from_os_str))]
+    input: PathBuf,
+
+    /// How many characters wide to make the image.
+    #[structopt(short = "w", long = "ascii-width", default_value = "60")]
+    ascii_width: u32,
+
+    /// The height of the font for the output image, in pixels.
+    #[structopt(short = "h", long = "font-height", default_value = "10.0")]
+    font_height: f32,
+
+    /// If specified, an image will be saved to the specified output path.
+    #[structopt(name = "OUTPUT", parse(from_os_str))]
+    output: Option<PathBuf>,
+}
 
 fn main() {
-    let args = env::args().collect::<Vec<_>>();
-    let image = image::open(&args[1])
+    let opt = Opt::from_args();
+    let image = image::open(opt.input)
         .expect("failed to load image")
         .to_rgb();
-    let chaxels = ascender::to_chaxels(&image, 60);
+    let chaxels = ascender::to_chaxels(&image, opt.ascii_width);
     print!("{}", ascender::output::to_256_terminal(&chaxels));
-    let bitmap = ascender::output::to_bitmap(&chaxels, 10.0).unwrap();
-
-    bitmap.save(&args[2]).unwrap();
+    if let Some(output) = opt.output {
+        let bitmap = ascender::output::to_bitmap(&chaxels, opt.font_height).unwrap();
+        bitmap.save(output).unwrap();
+    }
 }
