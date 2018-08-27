b176494735c8a85c5c98aca2efd72e138f19603b
Ash <relativistic.policeman@gmail.com>
2018-08-26 23:24:52 -0700

    js-beautify all the JS and CSS.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,32 +1,46 @@
-const { CheckerPlugin } = require('awesome-typescript-loader');
+const {
+  CheckerPlugin
+} = require('awesome-typescript-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
 
-    devtool: "source-map",
+  devtool: "source-map",
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
 
-    module: {
-        rules: [
-          { test: /\.(jpe?g|png|gif)$/, loader: "file-loader" },
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-          { test: /\.css$/, use: ["style-loader", "css-loader"]},
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+  module: {
+    rules: [{
+        test: /\.(jpe?g|png|gif)$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new CheckerPlugin()
-    ],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CheckerPlugin()
+  ],
 
-    mode: "development"
+  mode: "development"
 };
