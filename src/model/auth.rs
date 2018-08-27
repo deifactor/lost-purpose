7b76bb4fbfaa7b7875450007704a43814788ce3e
Ash <relativistic.policeman@gmail.com>
2018-08-26 23:19:40 -0700

    Delete all the old Rust code.
    Most of the logic that it was there for has been moved to the client. I'm going
    to eventually implement state saving, but by the time I get around to that I'll
    have released 1.0 and I don't want to do that with clutter in the repo.

--- src/model/auth.rs
+++
@@ -1,9 +0,0 @@
-use chrono::prelude::*;
-use schema::users;
-
-#[derive(Debug, Identifiable, Queryable, PartialEq, Eq)]
-pub struct User {
-    pub id: i32,
-    pub email: String,
-    created_at: DateTime<Utc>,
-}
