eff247610c7eee883a1969318d62871c7a884f0f
Ash <relativistic.policeman@gmail.com>
2018-09-09 11:43:48 -0700

    Use codecov for coverage.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
+  "collectCoverage": true,
   "coveragePathIgnorePatterns": [
     // Ignored because it has a bunch of really long arrays.
     "card_formatter.tsx"
