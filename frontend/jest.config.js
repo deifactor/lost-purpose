e48e50609601325d425557cf5a100c4b4d8ad484
Ash <relativistic.policeman@gmail.com>
2018-09-09 11:59:03 -0700

    Expand coverage to *all* ts/tsx files, to keep myself honest.
    Before it was just showing coverage on tested files, which is an easy metric to
    improve. Including elements is going to, hopefully, push me to actually test
    more.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   "collectCoverage": true,
+  "collectCoverageFrom": [
+    "src/**/*.{ts,tsx}",
+  ],
   "coveragePathIgnorePatterns": [
+    "/node_modules/",
+    "\\.d.ts$",
     // Ignored because it has a bunch of really long arrays.
     "card_formatter.tsx"
   ],
