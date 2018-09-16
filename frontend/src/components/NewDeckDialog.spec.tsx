d2d8471ab6095d5c9e9bf293cf85a66d42f20254
Ash <relativistic.policeman@gmail.com>
2018-09-15 22:30:01 -0700

    Add some basic tests for NewDeckDialog.

--- frontend/src/components/NewDeckDialog.spec.tsx
+++ frontend/src/components/NewDeckDialog.spec.tsx
@@ -2,10 +2,19 @@ import * as React from 'react';
 import * as Cards from '../cards/cards';
 import { NewDeckDialog } from './NewDeckDialog';
 import { shallow } from 'enzyme';
+import update from "immutability-helper";
 
 function ignore(deck: Cards.Deck) {}
 
-it("doesn't allow submission with an empty name", () => {
-  const wrapper = shallow(<NewDeckDialog onNewDeck={ignore} />);
-  expect(wrapper.find('button').prop('disabled')).toBeTruthy();
+describe("the submit button", () => {
+  it("doesn't allow submission with an empty name", () => {
+    const wrapper = shallow(<NewDeckDialog onNewDeck={ignore} />);
+    wrapper.instance().setState(state => update(state, { form: { name: { $set: "" } } }));
+    expect(wrapper.find('button').prop('disabled')).toBeTruthy();
+  });
+  it("allows submission with a nonempty name", () => {
+    const wrapper = shallow(<NewDeckDialog onNewDeck={ignore} />);
+    wrapper.instance().setState(state => update(state, { form: { name: { $set: "nonempty" } } }));
+    expect(wrapper.find('button').prop('disabled')).toBeFalsy();
+  });
 });
