630c342c7e5f13426c6fc11b037470c3e1a6e370
Ash <relativistic.policeman@gmail.com>
2018-09-29 20:29:33 -0700

    More tslint stuff.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import ReactModal = require('react-modal');
-import { HashRouter } from 'react-router-dom';
-import App from './components/app';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import ReactModal = require("react-modal");
+import { HashRouter } from "react-router-dom";
+import App from "./components/app";
 
 const el = document.createElement("div");
-el.id = 'app';
+el.id = "app";
 document.body.appendChild(el);
-ReactModal.setAppElement('#app');
+ReactModal.setAppElement("#app");
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, el);
